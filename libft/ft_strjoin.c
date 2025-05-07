70d19edce28f10f6c0d30a2026bf8fd5d3442447
zakaria jaddad <zakaria.jaddad26@gmail.com>
2025-05-07 16:59:05 +0100

    Refactor: ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: zajaddad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 00:44:38 by zajaddad          #+#    #+#             */
-/*   Updated: 2025/04/21 16:05:53 by zajaddad         ###   ########.fr       */
+/*   Updated: 2025/05/04 20:19:50 by zajaddad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len_s2;
 	char	*str;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+        if (s1 == NULL)
+                return ft_strdup(s2);
+        if (s2 == NULL)
+                return ft_strdup(s1);
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
 	str = (char *) malloc(len_s1 + len_s2 + 1);
