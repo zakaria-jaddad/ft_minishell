7f11153f04eed5caa8aaf3b92af1db554842b420
zakaria jaddad <zakaria.jaddad26@gmail.com>
2025-05-08 17:09:19 +0100

    Refactor: lstdelone now use a del function pointer

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: zajaddad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:42:58 by zajaddad          #+#    #+#             */
-/*   Updated: 2025/05/01 20:23:52 by zajaddad         ###   ########.fr       */
+/*   Updated: 2025/05/08 17:00:38 by zajaddad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (lst != NULL || del != NULL)
 	{
-		free(lst->content);
+		del(lst->content);
 		free(lst);
 	}
 }
