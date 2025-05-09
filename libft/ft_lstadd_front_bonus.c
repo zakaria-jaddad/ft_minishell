fa87895e6663b5020d9f08393a43ac1b44c9e9bb
Mouad Ait El Aouad <aitelaouadmoad@gmail.com>
2025-05-09 10:27:08 +0100

    just norm modifications and header updated for the ft_lst_rm_one

--- libft/ft_lstadd_front_bonus.c
+++ libft/ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: zajaddad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:22:01 by zajaddad          #+#    #+#             */
-/*   Updated: 2025/05/01 12:45:30 by zajaddad         ###   ########.fr       */
+/*   Updated: 2025/05/09 10:22:42 by mouait-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-        if (*lst != NULL)
-        {
-	        new->next = *lst;
-                (*lst)->prev = new;
-
-        }
+	if (*lst != NULL)
+	{
+		new->next = *lst;
+		(*lst)->prev = new;
+	}
 	*lst = new;
 }
--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -20,13 +20,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-        if (s1 == NULL)
-                return ft_strdup(s2);
-        if (s2 == NULL)
-                return ft_strdup(s1);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
-	str = (char *) malloc(len_s1 + len_s2 + 1);
+	str = (char *)malloc(len_s1 + len_s2 + 1);
 	if (str == NULL)
 		return (NULL);
 	(void)ft_strlcpy(str, s1, len_s1 + 1);
