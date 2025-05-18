67794e1069d914e87cbe0296ba5250fc2fff848f
Mouad Ait El Aouad <aitelaouadmoad@gmail.com>
2025-05-18 18:26:57 +0100

    add the function that add node in the middel of the t_list struct, and update some builtin functions
     Changes to be committed:
    	modified:   execution/_pwd_.c
    	new file:   execution/sort_envs.c
    	modified:   includes/execution.h
    	modified:   libft/Makefile
    	new file:   libft/ft_add_node.c
    	modified:   libft/libft.h

---
+++ execution/sort_envs.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_envs.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mouait-e <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 16:36:15 by mouait-e          #+#    #+#             */
+/*   Updated: 2025/05/13 16:39:16 by mouait-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/execution.h"
+
+t_list	*sort_envs(t_list *list)
+{
+	void	*content;
+	t_list	*tmp;
+
+	if (!list)
+		return (NULL);
+	while (list->next)
+	{
+		tmp = list->next;
+		while (tmp)
+		{
+			if (ft_strcmp(((t_env *)list->content)->key,
+					((t_env *)tmp->content)->key) > 0)
+			{
+				content = list->content;
+				list->content = tmp->content;
+				tmp->content = content;
+			}
+			tmp = tmp->next;
+		}
+		list = list->next;
+	}
+	while (list && list->prev)
+		list = list->prev;
+	return (list);
+}
--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: mouait-e <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 17:01:43 by mouait-e          #+#    #+#             */
-/*   Updated: 2025/05/09 09:22:39 by mouait-e         ###   ########.fr       */
+/*   Updated: 2025/05/13 16:38:43 by mouait-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 int		_echo_(char **args, int fd);
 int		_cd_(t_list *list, char **args);
-int		_pwd_(t_list *env);
+int		_pwd_(char *pwd);
 int		_export_(t_list *list, char **args);
 int		_env_(t_list *list);
 void	_exit_(char **args);
@@ -29,5 +29,6 @@ char	**list_to_double_pointer(t_list *env_list);
 int		count_envs(t_list *envs);
 void	free_double_pointer(void **p);
 void	_unset_(t_list *envs, char **args);
+t_list	*sort_envs(t_list *list);
 
 #endif
