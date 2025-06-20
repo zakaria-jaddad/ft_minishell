8ead7804fc4adbf9136ccb41d9c03b1a04d8823e
Mouad Ait El Aouad <mouait-e@e3r9p14.1337.ma>
2025-06-20 11:58:15 +0100

    the main function 1st mirge

--- minishell.c
+++ minishell.c
@@ -3,26 +3,23 @@
 #include "includes/minishell.h"
 #include "includes/parsing/ast.h"
 
-int	main(int _, char **__, char **env)
-{
-	char	*line;
-	t_cmd	*cmd;
-	t_list	*env_lst;
+int main(int _, char **__, char **env) {
+  char *line;
+  t_cmd *cmd;
+  t_list *env_lst;
 
-        
-	env_lst = envs_init(env, _, __);
-	if (env_lst == NULL)
-		return (EXIT_FAILURE);
-	while (true)
-	{
-		line = readline("Hello Shell >> ");
-		if (line == NULL)
-			break ;
-		cmd = parse_cmd(line, env_lst);
-                if (cmd == NULL)
-                        continue ;
-                print_cmd(cmd, 0);
-	}
-	free(line);
-	return (EXIT_SUCCESS);
+  env_lst = envs_init(env, _, __);
+  if (env_lst == NULL)
+    return (EXIT_FAILURE);
+  while (true) {
+    line = readline("Hello Shell >> ");
+    if (line == NULL)
+      break;
+    cmd = parse_cmd(line, env_lst);
+    if (cmd == NULL)
+      continue;
+    print_cmd(cmd, 0);
+  }
+  free(line);
+  return (EXIT_SUCCESS);
 }
