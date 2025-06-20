97ea439418c6bc56e87b055acbc8a38e99c65f69
Mouad Ait El Aouad <mouait-e@e3r9p14.1337.ma>
2025-06-20 12:01:36 +0100

    modied header file && fix some prototyps conflex

--- includes/execution.h
+++ includes/execution.h
@@ -11,24 +11,38 @@
 /* ************************************************************************** */
 
 #ifndef EXECUTION_H
-# define EXECUTION_H
+#define EXECUTION_H
 
-# include "minishell.h"
-# include <sys/wait.h> // wait()
-# include <unistd.h>
+#include "./parsing/parsing.h"
+#include <fcntl.h>    // open
+#include <sys/wait.h> // wait()
+#include <unistd.h>
 
-int		_echo_(char **args, int fd);
-int		_cd_(t_list *list, char **args);
-int		_pwd_(char *pwd);
-int		_export_(t_list *list, char **args);
-int		_env_(t_list *list);
-void	_exit_(char **args);
-void	print_error(char *err, char *value, char *message);
-t_list	*find_node(t_list *envs, char *key);
-char	**list_to_double_pointer(t_list *env_list);
-int		count_envs(t_list *envs);
-void	free_double_pointer(void **p);
-void	_unset_(t_list *envs, char **args);
-t_list	*sort_envs(t_list *list);
+typedef struct s_tree {
+  char *command;
+  t_list *arguments;
+  struct s_tree *left;
+  struct s_tree *right;
+  t_token_type type;
+  t_list *infile;
+} t_cmd;
+
+int _echo_(t_list *args, int fd);
+int _cd_(t_list *list, char **args);
+int _pwd_(char *pwd);
+int _export_(t_list *list, char **args);
+int _env_(t_list *list);
+void _exit_(char **args);
+void print_error(char *err, char *value, char *message);
+t_list *find_node(t_list *envs, char *key);
+char **list_to_double_pointer(t_list *env_list);
+int count_envs(t_list *envs);
+void free_double_pointer(void **p);
+void _unset_(t_list *envs, char **args);
+t_list *sort_envs(t_list *list);
+char **envs_list_to_double_pointer(t_list *env_list);
+t_list *expand(char **args, t_list *envs);
+char *manage_pwd(char *value);
+int execution(t_cmd *tree, t_list *env_list);
 
 #endif
