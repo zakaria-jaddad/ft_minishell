f66b403b4ca6eb38106e2406b49a99bf5767a940
zakaria jaddad <zajaddad@e3r9p8.1337.ma>
2025-06-22 02:04:16 +0100

    Expansion: Progress

--- minishell.c
+++ minishell.c
@@ -3,7 +3,9 @@
 #include "includes/parsing/ast.h"
 #include "includes/parsing/env.h"
 #include "includes/parsing/tokenize.h"
+#include "includes/parsing/wildcards.h"
 #include "libft/libft.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,17 +19,158 @@ void		expand_simple_cmd(char **command, char ***arguments,
 t_list	*get_enhanced_tokens(t_list *tokens, char *delim);
 void		expand_command(char **command, t_list *command_t, t_list *env);
 
+t_list *get_word_start(t_list *tokens)
+{
+	if (tokens == NULL)
+		return (NULL);
+	while (tokens &&  is_word(tokens->content) == true)
+	{
+		if (tokens->prev == NULL)
+			break ;
+		tokens = tokens->prev;
+	}
+	return (tokens);
+}
 
-bool check_if_next_has_wildcard(t_list *current_token)
+bool validate_word(t_list *tokens_word)
 {
 	t_token *token;
 
-	if (current_token == NULL)
-		return false;
-	token = current_token->content;
-	if (token == NULL)
-		return false;
-	return (ft_strchr(token->data, '*') != NULL);
+	while (tokens_word && tokens_word->content)
+	{
+		token = tokens_word->content;
+		if (check_token_type(token, TOKEN_WORD) == true)
+		{
+			tokens_word = tokens_word->next;
+			continue ;
+		}
+		if (ft_strcmp(token->data, "*") == 0)
+			return false;
+		tokens_word = tokens_word->next;
+	}
+	return true;
+}
+
+char *get_pre_expanded_file_name(t_list *tokens)
+{
+	t_token *token;
+	char *pre_expansion_name = NULL;
+
+	while (tokens && tokens->content && is_word(tokens->content) == true)
+	{
+		token = tokens->content;
+		append_str(&pre_expansion_name, token->data);
+		if (pre_expansion_name == NULL)
+			return (NULL);
+		tokens = tokens->next;
+	}
+	return (pre_expansion_name);
+}
+
+void 	remove_word_from_tokens(t_list **tokens)
+{
+	t_list *word_start;
+	t_list *node_to_del;
+
+	if (tokens == NULL || *tokens == NULL)
+		return ;
+	word_start = *tokens;
+	while (*tokens && !is_word((*tokens)->content))
+	{
+		node_to_del = *tokens;
+		(*tokens) = (*tokens)->next;
+		ft_lst_rm_one(node_to_del, free_token);
+	}
+}
+
+t_list *create_tokenized_filenames(t_list *filename)
+{
+	t_list *token_node;
+	t_list *tokenized_filenames;
+	if (filename == NULL)
+		return (NULL);
+	tokenized_filenames = NULL;
+	while (filename)
+	{
+		token_node = create_token_node(TOKEN_WORD, filename->content);
+		if (token_node == NULL)
+			return (ft_lstclear(&tokenized_filenames, free_token), NULL);
+		ft_lstadd_back(&tokenized_filenames, token_node);
+		if (append_tokens(&tokenized_filenames, TOKEN_WHITE_SPACE, " ") == NULL)
+			return (ft_lstclear(&tokenized_filenames, free_token), NULL);
+		filename = filename->next;
+	}
+	return (tokenized_filenames);
+}
+
+/* void append_filenames(t_list **tokens, t_list *tokenized_filenames) */
+/* { */
+/* 	t_list *start; */
+/* 	t_list *end; */
+/* 	t_list *node_to_del; */
+/**/
+/* 	print_tokens_data(tokenized_filenames); */
+/**/
+/* 	if (tokenized_filenames == NULL || tokens == NULL || *tokens == NULL)	 */
+/* 		return ; */
+/* 	start = (*tokens)->prev; */
+/* 	while (*tokens && is_word((*tokens)->content))  */
+/* 		*tokens = (*tokens)->next; */
+/* 	end = *tokens; */
+/* 	while (*tokens && *tokens != end) */
+/* 	{ */
+/* 		node_to_del = *tokens; */
+/* 		*tokens = (*tokens)->next; */
+/* 		ft_lst_rm_one(node_to_del, free_token); */
+/* 	} */
+/* 	*tokens = start->next; */
+/* 	start->next = NULL; */
+/* 	if (start != NULL) */
+/* 	{ */
+/* 		ft_lstadd_back(&start, tokenized_filenames); */
+/* 		ft_lstadd_back(&start, end); */
+/* 		*tokens = ft_lstfirst(start); */
+/* 		return ; */
+/* 	} */
+/* 	*tokens = tokenized_filenames; */
+/* } */
+
+void append_filenames(t_list **enhanced_tokens, t_list **tokens_word_start, t_list *tokenized_filenames)
+{
+	t_list *start = NULL;
+	t_list *end = NULL;
+	t_list *tmp;
+	t_list *curr;
+
+	if (!tokens_word_start || !*tokens_word_start || !tokenized_filenames)
+		return;
+	curr = *tokens_word_start;
+	start = curr->prev;
+	while (curr && is_word(curr->content))
+		curr = curr->next;
+	end = curr;
+	curr = (start) ? start->next : *tokens_word_start;
+	while (curr != end)
+	{
+		tmp = curr->next;
+		ft_lst_rm_one(curr, free_token);
+		curr = tmp;
+	}
+	if (start)
+	{
+
+		start->next = tokenized_filenames;
+		tokenized_filenames->prev = start;
+	}
+	else
+		*tokens_word_start = tokenized_filenames;
+	*enhanced_tokens = ft_lstlast(tokenized_filenames);
+	t_list *last = ft_lstlast(tokenized_filenames);
+	last->next = end;
+	if (end)
+		end->prev = last;
+	while ((*tokens_word_start)->prev)
+		*tokens_word_start = (*tokens_word_start)->prev;
 }
 
 t_list	*expand_me(t_list *tokens, t_list *env)
@@ -91,11 +234,49 @@ t_list	*expand_me(t_list *tokens, t_list *env)
 		enhanced_tokens = enhanced_tokens->next;
 	}
 
+	// Wildcard expanstion
+	enhanced_tokens = enhanced_tokens_head;
+	while (enhanced_tokens)
+	{
+		current_token = enhanced_tokens->content;
+		if (check_token_type(current_token, TOKEN_WORD) == true &&
+      			ft_strcmp(current_token->data, "*") != 0)
+		{
+			enhanced_tokens = enhanced_tokens->next;
+			continue ;
+		}
+		if (check_token_type(current_token, TOKEN_DOUBLE_QUOTE_WORD) == true || check_token_type(current_token, TOKEN_SINGLE_QUOTE_WORD) == true)
+		{
+			enhanced_tokens = enhanced_tokens->next;
+			continue ;
+		}
+		t_list *current_word = get_word_start(enhanced_tokens);
+		if (current_word == NULL)
+			break ;
+		if (validate_word(current_word) == false)
+		{
+			enhanced_tokens = enhanced_tokens->next;
+			continue ;
+		}
+		char *pre_expansion_name = get_pre_expanded_file_name(current_word);
+		if (pre_expansion_name == NULL)
+			return (ft_lstclear(&enhanced_tokens, free_token), NULL);
+		t_list *filenames = expand_wildcard(pre_expansion_name);
+		if (filenames == NULL)
+			return (ft_lstclear(&enhanced_tokens, free_token), NULL);
+		t_list *tokenized_filename = create_tokenized_filenames(filenames);
+		append_filenames(&enhanced_tokens, &current_word, tokenized_filename);
+		enhanced_tokens_head = ft_lstfirst(current_word);
+		if (enhanced_tokens != NULL)
+			enhanced_tokens = enhanced_tokens->next;
+		else
+			break ;
+	}
 	return (enhanced_tokens_head);
 }
 
 void	execution_mimic(t_cmd *cmd, t_list *env)
-{
+{ 
 	char			*command;
 	char			**arguments;
 	t_cmd_simple	*simple_command;
@@ -108,6 +289,7 @@ void	execution_mimic(t_cmd *cmd, t_list *env)
 		command = NULL;
 		arguments = NULL;
 		expand_command(&command, simple_command->command, env);
+		expand_arguments(&arguments, simple_command->arguments, env);
 		printf("cmd: %s\n", command);
 		printf("arguments : ");
 		fflush(stdout);
@@ -118,6 +300,17 @@ void	execution_mimic(t_cmd *cmd, t_list *env)
 			fflush(stdout);
 		}
 	}
+}
+
+void	expand_command(char **command, t_list *command_t, t_list *env)
+{
+	t_list	*expanded_command;
+
+	(void)command;
+	expanded_command = expand_me(command_t, env);
+	print_tokens(expanded_command);
+	printf("\n");
+	exit(0);
 }
 
 void	expand_command(char **command, t_list *command_t, t_list *env)
