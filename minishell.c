28a9e982dbcb2a5b42b92f6231c4bc3db5210995
zakaria jaddad <zakaria.jaddad26@gmail.com>
2025-07-14 17:04:06 +0100

    Minishell: Refacrot main

--- minishell.c
+++ minishell.c
@@ -1,53 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zajaddad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 17:02:50 by zajaddad          #+#    #+#             */
+/*   Updated: 2025/07/14 17:03:35 by zajaddad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "includes/minishell.h"
-#include "includes/parsing/parsing.h"
-#include "libft/libft.h"
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
 
-int	main(int _, char **__, char **env)
+static bool	is_valid_line(char *line)
 {
-	char			*line;
-	t_cmd			*cmd;
-	t_list			*env_lst;
-	struct termios	tr;
+	if (*line == 0)
+		return (false);
+	if (count_spaces(line) == (int)ft_strlen(line))
+		return (false);
+	return (true);
+}
 
-	env_lst = envs_init(env, _, __);
-	if (env_lst == NULL)
-		return (EXIT_FAILURE);
-	// Register signal handler for SIGINT (Ctrl+C)
+void	sig(void)
+{
 	if (signal(SIGINT, handle_ctr_c) == SIG_ERR)
 	{
 		ft_fprintf(STDERR_FILENO, "signal: error handling ctr+c!!\n");
-		return (1);
+		exit(EXIT_FAILURE);
 	}
 	signal(SIGQUIT, SIG_IGN);
-	if (!isatty(0) || !isatty(1) || !isatty(2))
-		return (EXIT_FAILURE);
+}
+
+void	ft_minishell(t_list *env)
+{
+	char			*line;
+	t_cmd			*cmd;
+	struct termios	tr;
+
 	while (true)
 	{
 		tcgetattr(STDERR_FILENO, &tr);
 		signals_handling();
 		line = readline("ft_minishell -> ");
 		if (line == NULL)
-			(ft_lstclear(&env_lst, free_env), _exit_(NULL));
-		if (*line)
+			(ft_lstclear(&env, free_env), _exit_(NULL));
+		if (is_valid_line(line) == true)
 			add_history(line);
-		cmd = parse_cmd(line, env_lst);
+		cmd = parse_cmd(line, env);
 		if (cmd == NULL)
 		{
 			line = (free(line), NULL);
 			continue ;
 		}
-		/* print_cmd(cmd, 0); */
-		status_x(execution(cmd, env_lst), 1);
+		status_x(execution(cmd, env), 1);
 		clear_cmd(cmd);
-		/* ft_lstclear(&env_lst, free_env); */
 		line = (free(line), NULL);
-		/* return (0); */
 		tcsetattr(STDERR_FILENO, TCSANOW, &tr);
 	}
-	free(line);
+}
+
+int	main(int _, char **__, char **env)
+{
+	t_list	*env_lst;
+
+	sig();
+	if (!isatty(0) || !isatty(1) || !isatty(2))
+		return (EXIT_FAILURE);
+	env_lst = envs_init(env, _, __);
+	if (env_lst == NULL)
+		return (EXIT_FAILURE);
+	ft_minishell(env_lst);
 	return (EXIT_SUCCESS);
 }
